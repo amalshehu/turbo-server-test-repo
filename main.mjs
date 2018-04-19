8e5ae66eb490b5c4b1d9b74f90ff1017f9e84edb
amalshehu <amalshehu@gmail.com>
2018-04-19 16:45:09 +0530

    Add more routes

--- main.mjs
+++ main.mjs
@@ -1,3 +1,22 @@
 import App from 'turbo-serv'
 
-console.log(App)
+const app = new App()
+const router = app.getRouter()
+
+router.get('/', function() {
+  this.res.send('Hello World')
+})
+
+router.get('/user', function() {
+  this.res.send(`Hi User!. Your id is ${this.param}`)
+})
+
+router.get('/user/edit', function() {
+  this.res.send(`Your id is ${this.param}. This is an edit route.`)
+})
+
+router.get('/user/delete', function() {
+  this.res.send(`Your id is ${this.param}. Delete user logic goes here.`)
+})
+
+app.listen(3000)
